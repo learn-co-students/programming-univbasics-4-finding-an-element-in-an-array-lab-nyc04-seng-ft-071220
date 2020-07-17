9690e949bcb650f228628c8fd36b5fe224e93161
Patrick Pierre <patrick.pierre000@gmail.com>
2020-07-17 22:50:58 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,13 @@
-def find_element_index(array, value_to_find)
-  # Add your solution here
+def find_element_index(array,value_to_find)
+  count = 0 #my counter
+  return_value = nil #the value I will use later in my implicit return_value
+  
+  while count < array.length do
+    if array[count] == value_to_find
+      return_value = count
+    end
+    count += 1 #update the count variable to represent the next index position 
+  end
+  #while loop is officially over
+  return_value #my implicit return
 end
